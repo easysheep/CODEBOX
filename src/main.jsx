64f160103187276e40e8663e4738de18710e39ef
easysheep <112809366+easysheep@users.noreply.github.com>
2024-06-30 20:47:41 +0530

    added dist

--- src/main.jsx
+++ src/main.jsx
@@ -1,46 +1,36 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
-import Editor from './Components/Editor/Editor.jsx'
-import Home from './Components/Home/Home.jsx'
-import Projects from './Components/Projects/Projects.jsx'
-import 'react-toastify/dist/ReactToastify.css';
-import { ToastContainer } from 'react-toastify';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Editor from "./Components/Editor/Editor.jsx";
+import Home from "./Components/Home/Home.jsx";
+import Projects from "./Components/Projects/Projects.jsx";
+import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer } from "react-toastify";
 const router = createBrowserRouter([
   {
     children: [
-      { path: "/", element: <Home/> },
-      { path: "/editor", element: <Editor/> },
-      { path: "/projects", element: <Projects/>}
-      
-  
-
-
-    ]
-  }
-
-
-
+      { path: "/", element: <Home /> },
+      { path: "/editor", element: <Editor /> },
+      { path: "/projects", element: <Projects /> },
+    ],
+  },
 ]);
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <>
-  
-
     <RouterProvider router={router}></RouterProvider>
     <ToastContainer
-position="top-center"
-autoClose={2000}
-hideProgressBar={false}
-newestOnTop={false}
-closeOnClick
-rtl={false}
-pauseOnFocusLoss
-draggable
-pauseOnHover
-theme="dark"
-transition: Slide
-/>
-   </>
-
-)
+      position="top-center"
+      autoClose={2000}
+      hideProgressBar={false}
+      newestOnTop={false}
+      closeOnClick
+      rtl={false}
+      pauseOnFocusLoss
+      draggable
+      pauseOnHover
+      theme="dark"
+      transition:Slide
+    />
+  </>
+);
